29c387f76d9d1defee1fd6705b355dcea6c0d065
yohannesmeasho <yohannes.measho@1nce.com>
2026-03-05 22:56:42 +0000

    auomates build

---
+++ lib/getArtworks.ts
@@ -0,0 +1,30 @@
+import fs from "fs";
+import path from "path";
+
+export type Artwork = {
+  id: string;
+  title: string;
+  image: string;
+  thumbnail: string;
+};
+
+export function getArtworks(): Artwork[] {
+  const dir = path.join(process.cwd(), "public/artworks/originals");
+
+  const files = fs.readdirSync(dir);
+
+  return files.map((file, i) => {
+    const name = file.replace(/\.[^/.]+$/, "");
+
+    const title = name
+      .replace(/-/g, " ")
+      .replace(/\b\w/g, l => l.toUpperCase());
+
+    return {
+      id: String(i),
+      title,
+      image: `artworks/originals/${file}`,
+      thumbnail: `artworks/thumbs/${file}`
+    };
+  });
+}
