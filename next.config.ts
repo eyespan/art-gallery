3b15b7ea585538133b930d00042731a83f2dc52a
yohannesmeasho <yohannes.measho@1nce.com>
2026-03-05 22:13:32 +0000

    highland

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/art-gallery",
+  assetPrefix: "/art-gallery/",
 };
 
 export default nextConfig;
+
