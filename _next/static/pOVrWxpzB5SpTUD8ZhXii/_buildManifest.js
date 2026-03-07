7b1fe3bbb278391fb86a032082b7c402b2eb4723
yohannesmeasho <yohannes.measho@1nce.com>
2026-03-07 01:22:43 +0000

    Updates

--- _next/static/pOVrWxpzB5SpTUD8ZhXii/_buildManifest.js
+++ _next/static/pOVrWxpzB5SpTUD8ZhXii/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/art-gallery//_next/:path+",
-        "destination": "/art-gallery/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
