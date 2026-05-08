5e5437845b61529579331dfd1d28f9439db88f63
yohannesmeasho <yohannes.measho@1nce.com>
2026-05-08 14:41:50 +0100

    Fix custom domain paths

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+/*import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
@@ -8,6 +8,18 @@ const nextConfig: NextConfig = {
   },
   basePath: "/art-gallery",
   assetPrefix: "/art-gallery",
+};
+
+export default nextConfig; */
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
